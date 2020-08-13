45bd449ee91b3080c21fa50a81b110a69c3921ca
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-12 21:32:59 -0300

    Add create-user migration & User model

---
+++ server/src/app/models/User.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+class User extends Sequelize.Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      },
+    );
+  }
+}
+
+export default User;
