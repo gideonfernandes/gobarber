792b130ea5f93e4ea762e5321b8327bba621b508
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-14 23:09:16 -0300

    Add multer setup

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,15 +1,23 @@
 const { Router } = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const authMiddleware = require('./app/middlewares/auth');
 
 const routes = new Router();
+const uploadMiddleware = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post(
+  '/files',
+  uploadMiddleware.single('file'),
+  (request, response) => response.json({ ok: true }),
+);
 
 module.exports = routes;
