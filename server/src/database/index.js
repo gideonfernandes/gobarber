cbb4bc50ad4c35a95d5599eb614a7af6949e6ea8
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-15 13:57:00 -0300

    Add mongoDB setup

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const Mongoose = require('mongoose');
 const databaseConfig = require('../config/database');
 const User = require('../app/models/User');
 const File = require('../app/models/File');
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -17,6 +19,17 @@ class Database {
     models
       .map((model) => model.init(this.connection))
       .map((model) => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = Mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      },
+    );
   }
 }
 
