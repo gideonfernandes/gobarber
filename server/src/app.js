df4990424640b9e3ea6ed078f42f59687239da1e
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-17 15:56:11 -0300

    Add exception handling with sentry, youch & express-async-errors

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const path = require('path');
+const Youch = require('youch');
+const Sentry = require('@sentry/node');
+require('express-async-errors');
 const routes = require('./routes');
+const sentryConfig = require('./config/sentry');
 require('./database');
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +28,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, request, response, next) => {
+      const errors = await new Youch(err, request).toJSON();
+
+      return response.status(500).json(errors);
+    });
   }
 }
 
---
+++ server/src/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dsn: 'https://7e4cd6f583414e23a19a3ab41a235699@o389886.ingest.sentry.io/5394384',
+};
