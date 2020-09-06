3eb9fe69f15c9b3a0d51843a731080f9e4b84e33
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-06 10:00:24 -0300

    Add auth pages style

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import logo from '~/assets/logo.svg';
+import { Link } from 'react-router-dom';
+
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber Logo" />
+
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  )
 };
--- web/src/pages/_layouts/auth/styles.js
+++ web/src/pages/_layouts/auth/styles.js
@@ -1,6 +1,62 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #7159C1, #AB56C1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #FFF;
+      margin-bottom: 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    button {
+      margin-top: 5px;
+      height: 44px;
+      background: #3B9EFF;
+      color: #FFF;
+      font-weight: bold;
+      border: 0;
+      border-radius: 4px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#3B9EFF')};
+      }
+    }
+
+    a {
+      color: #FFF;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
 `;
