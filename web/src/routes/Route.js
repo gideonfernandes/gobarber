efcdaabca0f8ee4ce31c0e7da4650e1eca73a9bf
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-06 18:57:17 -0300

    Add auth login actions

--- web/src/routes/Route.js
+++ web/src/routes/Route.js
@@ -5,19 +5,21 @@ import PropTypes from 'prop-types';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
-    return <Redirect to={{ pathname: '/' }} />;
+    return <Redirect push to={{ pathname: '/' }} />;
   };
 
   if(signed && !isPrivate) {
-    return <Redirect to={{ pathname: '/dashboard' }} />;
+    return <Redirect push to={{ pathname: '/dashboard' }} />;
   };
 
   const Layout = signed ? DefaultLayout : AuthLayout;
